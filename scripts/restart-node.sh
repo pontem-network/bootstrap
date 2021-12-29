4c6e7b66cc408dc9932838dd06e7417165aa4060
Artsiom Holikau <artsiom@dfinance.co>
2021-12-28 20:13:48 -0500

    fix script

--- scripts/restart-node.sh
+++ scripts/restart-node.sh
@@ -2,17 +2,16 @@
 
 while true; do
     LAST_LINE="200"
-    FIND_ERRORS="
-    Thread 'tokio-runtime-worker' panicked at 'Storage root must match that calculated.'
-    "
-    IFS=$'\n'
+    declare -a FIND_ERRORS=("Thread 'tokio-runtime-worker' panicked at 'Storage root must match that calculated.'")
+    #IFS=$'\n'
 
     docker logs pontem-node -n ${LAST_LINE} >& pontem-node.log
 
     # set -x
-    for error in ${FIND_ERRORS}; do
+    for error in "${FIND_ERRORS[@]}"; do
         if grep -iq "${error}" pontem-node.log; then
             echo 'The error was found.'
+            echo "${error}"
             docker container restart pontem-node
         fi
     done
